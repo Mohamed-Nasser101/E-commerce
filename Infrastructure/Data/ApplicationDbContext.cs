a4debca25c45027bad9501342a78da75aa0e671b
Mohamed Nasser <mohamed.nasser3010@gmail.com>
2021-10-22 14:42:04 +0200

    Initial

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
